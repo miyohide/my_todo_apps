83cebeea697ba5b6f63a521d7e9bfea397ae8608
Hidenori Miyoshi <miyohide@gmail.com>
2023-03-14 20:30:18 +0900

    create todo table

--- db/migrate/20230314112830_create_todos.rb
+++ db/migrate/20230314112830_create_todos.rb
@@ -1,10 +1,10 @@
 class CreateTodos < ActiveRecord::Migration[7.0]
   def change
     create_table :todos do |t|
-      t.string :name
-      t.date :start
-      t.date :end
-      t.integer :progress
+      t.string :name, null: false
+      t.date :start, null: false
+      t.date :end, null: false
+      t.integer :progress, null: false, default: 0
 
       t.timestamps
     end
