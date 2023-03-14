8be2aef8cd2a484f69725a809125d525543c1c55
Hidenori Miyoshi <miyohide@gmail.com>
2023-03-14 20:30:25 +0900

    run db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_06_095909) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_14_112830) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "todos", force: :cascade do |t|
+    t.string "name", null: false
+    t.date "start", null: false
+    t.date "end", null: false
+    t.integer "progress", default: 0, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
