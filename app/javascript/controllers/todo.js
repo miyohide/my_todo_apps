b1eb5bd58892f5ab23c96d65dc0288179caf3162
Hidenori Miyoshi <miyohide@gmail.com>
2023-03-15 20:58:14 +0900

    get data from api using axios

--- app/javascript/controllers/todo.js
+++ app/javascript/controllers/todo.js
@@ -1,3 +1,16 @@
+import axios from 'axios';
+
 export default function showGantt() {
   console.log('show Gantto');
+  todos();
+}
+
+function todos() {
+  axios.get('/todos.json')
+    .then(function (res) {
+     console.log(res.data);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
 }
