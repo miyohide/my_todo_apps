d985540a3b239c3cdd668345e0c75cb35bff44f1
Hidenori Miyoshi <miyohide@gmail.com>
2023-03-16 20:33:40 +0900

    rails g stimulus showGantt

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,4 +2,7 @@
 // Run that command whenever you add a new controller or create them with
 // ./bin/rails generate stimulus controllerName
 
-import { application } from "./application";
+import { application } from "./application"
+
+import ShowGanttController from "./show_gantt_controller"
+application.register("show-gantt", ShowGanttController)
