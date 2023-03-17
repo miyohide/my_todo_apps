260689307e85ab569576324a5a4fe9f26e1b188a
Hidenori Miyoshi <miyohide@gmail.com>
2023-03-17 20:55:11 +0900

    print out console log

--- app/javascript/controllers/show_gantt_controller.js
+++ app/javascript/controllers/show_gantt_controller.js
@@ -3,5 +3,6 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="show-gantt"
 export default class extends Controller {
   connect() {
+    console.log("hogehoge");
   }
 }
