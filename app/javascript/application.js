6fe7f988e4c660bc9eed385db7081b4ba07e3877
Hidenori Miyoshi <miyohide@gmail.com>
2023-03-17 21:01:26 +0900

    get json data using stimulus

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,10 +2,3 @@
 import "@hotwired/turbo-rails";
 import "./controllers";
 import * as bootstrap from "bootstrap"; // eslint-disable-line no-unused-vars
-import showGantt from "./controllers/todo";
-
-const todoIndex = document.getElementById('todos_index');
-
-if (todoIndex) {
-  showGantt();  
-};
--- app/javascript/controllers/show_gantt_controller.js
+++ app/javascript/controllers/show_gantt_controller.js
@@ -1,8 +1,19 @@
 import { Controller } from "@hotwired/stimulus"
+import axios from 'axios';
 
 // Connects to data-controller="show-gantt"
 export default class extends Controller {
   connect() {
-    console.log("hogehoge");
+    this.todos();
   }
+
+  todos() {
+    axios.get('/todos.json')
+      .then(function (res) {
+       console.log(res.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }  
 }
