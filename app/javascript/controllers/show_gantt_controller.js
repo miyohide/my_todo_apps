fcbb1bd1a808945e697dc1f2209607c08faa01d9
Hidenori Miyoshi <miyohide@gmail.com>
2023-03-18 12:58:54 +0900

    display gantt chart

--- app/javascript/controllers/show_gantt_controller.js
+++ app/javascript/controllers/show_gantt_controller.js
@@ -1,5 +1,6 @@
 import { Controller } from "@hotwired/stimulus"
 import axios from 'axios';
+import Gantt from "frappe-gantt";
 
 // Connects to data-controller="show-gantt"
 export default class extends Controller {
@@ -11,6 +12,7 @@ export default class extends Controller {
     axios.get('/todos.json')
       .then(function (res) {
        console.log(res.data);
+       new Gantt('#gantt', res.data);
       })
       .catch(function (error) {
         console.log(error);
