8e8a765181dc8d91cf0075f0d3adafa1b1afde7b
Hidenori Miyoshi <miyohide@gmail.com>
2023-03-21 17:11:49 +0900

    create 100 todo

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+100.times do |i|
+  Todo.create(name: "task #{i}", start: Date.today + i, end: Date.today + 10 + i, progress: 0)
+end
