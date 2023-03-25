b7d948c7e8f46f96c3486209e78c8dde5402f310
Hidenori Miyoshi <miyohide@gmail.com>
2023-03-25 10:35:51 +0900

    fix comparison statements

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,7 +1,7 @@
 class Todo < ApplicationRecord
   validates :name, presence: true, length: { maximum: 100 }
   validates :start, presence: true
-  validates :end, presence: true, comparison: { greater_than: :start }
+  validates :end, presence: true, comparison: { greater_than_or_equal_to: :start }
   validates :progress, presence: true, numericality: { only_integer: true }
 
   def self.ransackable_attributes(auth_object = nil)
