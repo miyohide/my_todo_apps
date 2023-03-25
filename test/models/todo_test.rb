185de3fcdd63f0d61a77ae3fd9ca814dd7dd6360
Hidenori Miyoshi <miyohide@gmail.com>
2023-03-25 13:20:07 +0900

    Add todo model test

--- test/models/todo_test.rb
+++ test/models/todo_test.rb
@@ -1,7 +1,33 @@
 require "test_helper"
 
 class TodoTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test '値が妥当な場合は保存できること' do
+    t = Todo.new(name: 'task1', start: Date.today, end: Date.today + 1, progress: 10)
+    assert t.save
+  end
+
+  test 'nameが入力されていないときには、保存できないこと' do
+    t = Todo.new(start: Date.today, end: Date.today + 1, progress: 10)
+    assert_not t.save
+  end
+
+  test 'nameが101文字以上入力されていないときには、保存できないこと' do
+    t = Todo.new(name: 'a'*101, start: Date.today, end: Date.today + 1, progress: 10)
+    assert_not t.save
+  end
+
+  test '開始日付が入力されていない時には、保存ができないこと' do
+    t = Todo.new(name: 'task1', end: Date.today + 1, progress: 10)
+    assert_not t.save
+  end
+
+  test '開始日付がフォーマット不正な場合には、保存ができないこと' do
+    t = Todo.new(name: 'task1', start: '20230412', end: Date.today + 1, progress: 10)
+    assert_not t.save
+  end
+
+  test '開始日付が日付として不正な場合には、保存ができないこと' do
+    t = Todo.new(name: 'task1', start: '2023-04-32', end: Date.today + 1, progress: 10)
+    assert_not t.save
+  end
 end
