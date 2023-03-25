b2b37b847905a884b38b2ca03b590953a4c48cb1
Hidenori Miyoshi <miyohide@gmail.com>
2023-03-25 10:33:16 +0900

    add validations

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,4 +1,9 @@
 class Todo < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 100 }
+  validates :start, presence: true
+  validates :end, presence: true, comparison: { greater_than: :start }
+  validates :progress, presence: true, numericality: { only_integer: true }
+
   def self.ransackable_attributes(auth_object = nil)
     ["end", "id", "name", "progress", "start"]
   end
