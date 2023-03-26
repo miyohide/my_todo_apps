61c1e4058d870a3e8612a5f402f952ec757b60e8
Hidenori Miyoshi <miyohide@gmail.com>
2023-03-26 18:35:40 +0900

    implement debounce

--- app/javascript/controllers/todo_controller.js
+++ app/javascript/controllers/todo_controller.js
@@ -2,6 +2,13 @@ import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
   submit() {
-    this.element.requestSubmit()
+    // セットされているTimeoutをクリアする
+    clearTimeout(this.timeout)
+
+    // Timeoutをセットする。200ms後にリクエストを実行する
+    // debounceを実装というらしい
+    this.timeout = setTimeout(() => {
+      this.element.requestSubmit()
+    }, 500)
   }
 }
