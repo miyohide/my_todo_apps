a69ccdbe2fb8a0a299a667feb6ba896695f21385
Hidenori Miyoshi <miyohide@gmail.com>
2023-04-12 06:42:20 +0900

    add todo_json_for_gantt helper test.

---
+++ test/helpers/todos_helper_test.rb
@@ -0,0 +1,14 @@
+require "test_helper"
+
+class TodosHelperTest < ActionView::TestCase
+  test 'todo_json_for_gantt はidがfrappe-gantt用のTodoのJSON文字列を返すこと' do
+    t1 = todos(:one)
+    t2 = todos(:two)
+    json_obj = JSON.parse(todo_json_for_gantt([t1, t2]))
+
+    assert_equal "frappe_gantt_id-#{t1.id}", json_obj[0]["id"]
+    assert_equal t1.name, json_obj[0]["name"]
+    assert_equal "frappe_gantt_id-#{t2.id}", json_obj[1]["id"]
+    assert_equal t2.name, json_obj[1]["name"]
+  end
+end
