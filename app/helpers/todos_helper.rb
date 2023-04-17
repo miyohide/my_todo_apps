c48d0f39f583fcb7aa09a8c759bbf93ef45d29af
Hidenori Miyoshi <miyohide@gmail.com>
2023-04-17 21:30:29 +0900

    add comment for TodosHelper

--- app/helpers/todos_helper.rb
+++ app/helpers/todos_helper.rb
@@ -1,4 +1,7 @@
+# TodosHelper はTodo viewに対するHelperメソッドを持つmodule
 module TodosHelper
+  # todo_json_for_gantt はFrappe-Gantt向けのJSONデータを生成する
+  # Helperメソッド
   def todo_json_for_gantt(todos)
     todos.map(&:frappe_gantt_todo).to_json
   end
