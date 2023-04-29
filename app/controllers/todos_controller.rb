936dfa0bc27cc440089a7294d80c63c7de510d9e
Hidenori Miyoshi <miyohide@gmail.com>
2023-04-29 20:55:11 +0900

    set 100% progress when checkbox is true

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -21,6 +21,7 @@ class TodosController < ApplicationController
   # POST /todos or /todos.json
   def create
     @todo = Todo.new(todo_params)
+    @todo.progress = 100 if params[:complete]
 
     respond_to do |format|
       if @todo.save
